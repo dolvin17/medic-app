a34fdd8410150fe4e5940b9af688f99ebd5a7f6f
dolvin17 <huertas876@gmail.com>
2025-10-09 01:06:13 +0200

    refactor(ui): Extract postal code display logic to CodigoPostal component

---
+++ src/components/CodigoPostal.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { getPostalInfo } from "@/lib/getPostalInfo";
+import { PostalData } from "@/types/postal";
+
+export default function CodigoPostal() {
+  const [postalData, setPostalData] = useState<PostalData | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    async function fetchData() {
+      setIsLoading(true);
+      const cp = "28036";
+      const result: PostalData[] | null = await getPostalInfo(cp);
+
+      if (result && result.length > 0) {
+        setPostalData(result[0]);
+      } else {
+        setPostalData(null);
+      }
+      setIsLoading(false);
+    }
+    fetchData();
+  }, []);
+  return (
+    <main>
+      <h1>Prueba de consulta</h1>
+      {isLoading && <p>Cargando info del CP</p>}
+      {!isLoading && !postalData && <p>No se encuentran datos</p>}
+      {!isLoading && postalData && (
+        <div>
+          <h2> Info encontrada</h2>
+          <p>
+            <strong>CP: </strong>
+            {postalData.codigo_postal}
+          </p>
+          <p>
+            <strong>Barrio:</strong> {postalData.nombre_barrio}
+          </p>
+		  <p><strong>Zona SER: </strong>{postalData.zona_ser.toString()}</p>
+        </div>
+      )}
+    </main>
+  );
+}
