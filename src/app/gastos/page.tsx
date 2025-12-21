47139a744ad4f0ffdbff428284125a19acd61e6a
dolvin17 <huertas876@gmail.com>
2025-12-21 14:07:04 +0100

    adding navbar

--- src/app/gastos/page.tsx
+++ src/app/gastos/page.tsx
@@ -1,9 +1,11 @@
 import ExpenseManager from "@/components/ExpenseMaganer";
 import FixedCostsManager from "@/components/FixedCostsManager";
+import Navigation from "@/components/navigation/Navigation";
 
 export default function Page() {
 	return (
 		<>
+		<Navigation/>
 		<div className="p-4">
 		<ExpenseManager/>
 		<FixedCostsManager/>
